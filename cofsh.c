335134a4720cf708c43ac2c3fd00c6ffa6d98068
Euan Bourke <euanbourke@gmail.com>
2026-02-12 12:39:46 +0000

    cofsh: add basic input parsing
    parses input string into args
    
    Signed-off-by: Euan Bourke <euanbourke@gmail.com>

--- cofsh.c
+++ cofsh.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define BUF_SIZE 64
+#define TOK_DELIM " "
 
 void startup_print() {
     char *startup_msg = 
@@ -7,20 +11,42 @@ void startup_print() {
     printf("%s\n", startup_msg);
 }
 
+char **parse_line(char *line) {
+    char **tokens = malloc(BUF_SIZE * sizeof(char *));
+    char *token;
+    int pos = 0;
+
+    if (!tokens) {
+        fprintf(stderr, "failed to malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, TOK_DELIM);
+    while (token != NULL) {
+//        printf("%d | %s\n", pos, token);
+        tokens[pos] = token;
+        pos++;
+
+        token = strtok(NULL, TOK_DELIM);
+    }
+    return tokens;
+}
+
 void cofsh_loop() {
-    char* line;
-    char** args;
+    char *line = NULL;
+    char **args;
     size_t len = 0;
-    int read;
     int status = 1;
 
     do {
         printf("> ");
 
-        read = getline(&line, &len, stdin);
-        if (read == -1) {
+        if (getline(&line, &len, stdin) == -1) {
             return;
         }
+        args = parse_line(line);
+        free(args);
+
     } while (status);
 }
 
@@ -32,3 +58,4 @@ int main() {
 
     return 0;
 }
+
