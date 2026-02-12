9e67184cf5bc4e6f7e98c310a87bbb525a24bfcd
Euan Bourke <euanbourke@gmail.com>
2026-02-12 14:25:24 +0000

    cofsh: add arg exec (builtin) and return argc from parse
    - add basic function to execute args. Currently only checks for builtins
    - add template for exit builtin (does not exit yet)
    - restructure arg parsing to pass the address to the arg "array" as
      parameter and return the arg count
    
    Signed-off-by: Euan Bourke <euanbourke@gmail.com>

--- cofsh.c
+++ cofsh.c
@@ -3,7 +3,27 @@
 #include <string.h>
 
 #define BUF_SIZE 64
-#define TOK_DELIM " "
+#define TOK_DELIM " \n"
+
+
+typedef int (*builtin_cmd)(int argc, char **args);
+
+int cofsh_exit(int argc, char **args);
+
+struct builtin_entry {
+    char *cmd;
+    builtin_cmd func;
+};
+
+struct builtin_entry builtins[] = {
+    {"exit", cofsh_exit},
+
+};
+ 
+int cofsh_exit(int argc, char **args) {
+    printf("EXIT CALL\n");
+    return 1; 
+}
 
 void startup_print() {
     char *startup_msg = 
@@ -11,12 +31,12 @@ void startup_print() {
     printf("%s\n", startup_msg);
 }
 
-char **parse_line(char *line) {
-    char **tokens = malloc(BUF_SIZE * sizeof(char *));
+int parse_line(char *line, char ***args) {
+    *args = malloc(BUF_SIZE * sizeof(char *));
     char *token;
     int pos = 0;
 
-    if (!tokens) {
+    if (!(*args)) {
         fprintf(stderr, "failed to malloc");
         exit(EXIT_FAILURE);
     }
@@ -24,17 +44,33 @@ char **parse_line(char *line) {
     token = strtok(line, TOK_DELIM);
     while (token != NULL) {
 //        printf("%d | %s\n", pos, token);
-        tokens[pos] = token;
+        (*args)[pos] = token;
         pos++;
 
         token = strtok(NULL, TOK_DELIM);
     }
-    return tokens;
+    return pos;
+}
+
+int exec_args(int argc, char **args) {
+
+    //first check for builtins
+    int num_builtins = sizeof(builtins) / sizeof(builtins[0]);
+    for (int i = 0; i < num_builtins; i++) {
+        printf("command: %s | builtin: %s\n", args[0], builtins[i].cmd);
+        if (strcmp(args[0], builtins[i].cmd) == 0) {
+            return builtins[i].func(1, args);
+        }
+    }    
+
+    //otherwise execute
+
 }
 
 void cofsh_loop() {
     char *line = NULL;
-    char **args;
+    char **args = NULL;
+    int *arg_count = NULL;
     size_t len = 0;
     int status = 1;
 
@@ -44,7 +80,9 @@ void cofsh_loop() {
         if (getline(&line, &len, stdin) == -1) {
             return;
         }
-        args = parse_line(line);
+        int ret = parse_line(line, &args);
+        printf("ret: %d\n", ret);
+        status = exec_args(ret, args);
         free(args);
 
     } while (status);
